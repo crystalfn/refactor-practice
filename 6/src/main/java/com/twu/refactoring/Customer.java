f8258a3013308a3b8d4035d37a59e7afb7521e86
crystalfn <969274162@qq.com>
2020-09-15 00:18:30 +0800

    [fangni] feat: practice6

--- 6/src/main/java/com/twu/refactoring/Customer.java
+++ 6/src/main/java/com/twu/refactoring/Customer.java
@@ -5,66 +5,81 @@ import java.util.Iterator;
 
 public class Customer {
 
-	private String name;
-	private ArrayList<Rental> rentalList = new ArrayList<Rental>();
+    private final String name;
+    private final ArrayList<Rental> rentalList = new ArrayList<Rental>();
 
-	public Customer(String name) {
-		this.name = name;
-	}
+    public Customer(String name) {
+        this.name = name;
+    }
 
-	public void addRental(Rental arg) {
-		rentalList.add(arg);
-	}
+    public void addRental(Rental rental) {
+        rentalList.add(rental);
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		Iterator<Rental> rentals = rentalList.iterator();
-		String result = "Rental Record for " + getName() + "\n";
-		while (rentals.hasNext()) {
-			double thisAmount = 0;
-			Rental each = rentals.next();
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Iterator<Rental> rentals = rentalList.iterator();
+        StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
+        while (rentals.hasNext()) {
+            double eachLineAmount = 0;
+            Rental each = rentals.next();
+            eachLineAmount = getEachLineAmount(eachLineAmount, each);
 
-			// determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
+            frequentRenterPoints++;
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
+                frequentRenterPoints++;
+            }
 
-			}
+            result.append("\t").append(each.getMovie().getTitle())
+                .append("\t").append(eachLineAmount)
+                .append("\n");
+            totalAmount += eachLineAmount;
+        }
 
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
+        result.append("Amount owed is ").append(totalAmount).append("\n");
+        result.append("You earned ").append(frequentRenterPoints)
+            .append(" frequent renter points");
+        return result.toString();
+    }
 
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+    private double getEachLineAmount(double eachLineAmount, Rental each) {
+        switch (each.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                eachLineAmount = getAmountCaseRegular(eachLineAmount, each);
+                break;
+            case Movie.NEW_RELEASE:
+                eachLineAmount = getAmountCaseNewRelease(eachLineAmount, each);
+                break;
+            case Movie.CHILDREN:
+                eachLineAmount = getAmountCaseChildren(eachLineAmount, each);
+                break;
+        }
+        return eachLineAmount;
+    }
 
-		}
-		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
-				+ " frequent renter points";
-		return result;
-	}
+    private double getAmountCaseRegular(double eachLineAmount, Rental each) {
+        eachLineAmount += 2;
+        if (each.getDaysRented() > 2) {
+            eachLineAmount += (each.getDaysRented() - 2) * 1.5;
+        }
+        return eachLineAmount;
+    }
 
+    private double getAmountCaseNewRelease(double eachLineAmount, Rental each) {
+        eachLineAmount += each.getDaysRented() * 3;
+        return eachLineAmount;
+    }
+
+    private double getAmountCaseChildren(double eachLineAmount, Rental each) {
+        eachLineAmount += 1.5;
+        if (each.getDaysRented() > 3) {
+            eachLineAmount += (each.getDaysRented() - 3) * 1.5;
+        }
+        return eachLineAmount;
+    }
 }
--- 6/src/test/java/com/twu/refactoring/CustomerTest.java
+++ 6/src/test/java/com/twu/refactoring/CustomerTest.java
@@ -7,7 +7,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
-import java.net.URL;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -17,13 +16,13 @@ public class CustomerTest {
 
 	private static final String GOLD_PATH = "data/";
 
-    private Customer dinsdale = new Customer("Dinsdale Pirhana");
+    private final Customer dinsdale = new Customer("Dinsdale Pirhana");
 
-    private Movie python = new Movie("Monty Python and the Holy Grail", Movie.REGULAR);
-	private Movie ran = new Movie("Ran", Movie.REGULAR);
-	private Movie la = new Movie("LA Confidential", Movie.NEW_RELEASE);
-	private Movie trek = new Movie("Star Trek 13.2", Movie.NEW_RELEASE);
-	private Movie wallace = new Movie("Wallace and Gromit", Movie.CHILDRENS);
+    private final Movie python = new Movie("Monty Python and the Holy Grail", Movie.REGULAR);
+	private final Movie ran = new Movie("Ran", Movie.REGULAR);
+	private final Movie la = new Movie("LA Confidential", Movie.NEW_RELEASE);
+	private final Movie trek = new Movie("Star Trek 13.2", Movie.NEW_RELEASE);
+	private final Movie wallace = new Movie("Wallace and Gromit", Movie.CHILDREN);
 
     @BeforeEach
     public void setUpData(){
