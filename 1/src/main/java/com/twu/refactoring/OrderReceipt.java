de7284ce533bdb675b2d0aeca8b59b4a9a5ba5d0
crystalfn <969274162@qq.com>
2020-09-14 21:22:04 +0800

    [fangni] refactor: refactor practice1 - OrderReceipt class

--- 1/src/main/java/com/twu/refactoring/OrderReceipt.java
+++ 1/src/main/java/com/twu/refactoring/OrderReceipt.java
@@ -1,57 +1,47 @@
 package com.twu.refactoring;
 
-/**
- * OrderReceipt prints the details of order including customer name, address, description, quantity,
- * price and amount. It also calculates the sales tax @ 10% and prints as part
- * of order. It computes the total order amount (amount of individual lineItems +
- * total sales tax) and prints it.
- * 
- */
 public class OrderReceipt {
-    private Order o;
+    private final Order order;
 
-    public OrderReceipt(Order o) {
-        this.o = o;
+    public OrderReceipt(Order order) {
+        this.order = order;
 	}
 
 	public String printReceipt() {
 		StringBuilder output = new StringBuilder();
+		String header = "======Printing Orders======\n";
+		output.append(header);
+        output.append(order.getCustomerName());
+        output.append(order.getCustomerAddress());
 
-		// print headers
-		output.append("======Printing Orders======\n");
+		printLineItems(output);
+		return output.toString();
+	}
 
-		// print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
-        output.append(o.getCustomerName());
-        output.append(o.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
+	private void printLineItems(StringBuilder output) {
+		double totalSalesTax = 0d;
+		double totalAmount = 0d;
+		for (LineItem lineItem : order.getLineItems()) {
+			appendStringWithHT(output, lineItem.getDescription());
+			appendStringWithHT(output, String.valueOf(lineItem.getPrice()));
+			appendStringWithHT(output, String.valueOf(lineItem.getQuantity()));
+			appendStringWithHT(output, String.valueOf(lineItem.totalAmount()));
 
-		// prints lineItems
-		double totSalesTx = 0d;
-		double tot = 0d;
-		for (LineItem lineItem : o.getLineItems()) {
-			output.append(lineItem.getDescription());
-			output.append('\t');
-			output.append(lineItem.getPrice());
-			output.append('\t');
-			output.append(lineItem.getQuantity());
-			output.append('\t');
-			output.append(lineItem.totalAmount());
-			output.append('\n');
-
-			// calculate sales tax @ rate of 10%
-            double salesTax = lineItem.totalAmount() * .10;
-            totSalesTx += salesTax;
-
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
-            tot += lineItem.totalAmount() + salesTax;
+			double taxRate = 0.10d;
+            double salesTax = lineItem.totalAmount() * taxRate;
+            totalSalesTax += salesTax;
+            totalAmount += lineItem.totalAmount() + salesTax;
 		}
 
-		// prints the state tax
-		output.append("Sales Tax").append('\t').append(totSalesTx);
+		appendStringWithHT(output, "Sales Tax");
+		output.append(totalSalesTax);
 
-        // print total amount
-		output.append("Total Amount").append('\t').append(tot);
-		return output.toString();
+		appendStringWithHT(output, "Total Amount");
+		output.append(totalAmount);
+	}
+
+	private void appendStringWithHT(StringBuilder output, String string) {
+		output.append(string);
+		output.append('\t');
 	}
 }
