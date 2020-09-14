19ce2c2cd8290853bd92c9f515054e7b28f58482
crystalfn <969274162@qq.com>
2020-09-15 00:27:40 +0800

    [fangni] refactor: practice6

--- 5/src/main/java/com/twu/refactoring/Receipt.java
+++ 5/src/main/java/com/twu/refactoring/Receipt.java
@@ -20,20 +20,26 @@ public class Receipt {
     public double getTotalCost() {
         double totalCost = 0;
 
-        // fixed charges
         totalCost += FIXED_CHARGE;
 
-        // taxi charges
         int totalKms = taxi.getTotalKms();
         double peakTimeMultiple = taxi.isPeakTime() ? PEAK_TIME_MULTIPLIER : OFF_PEAK_MULTIPLIER;
         if(taxi.isAirConditioned()) {
-            totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_AC_RATE * peakTimeMultiple;
-            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_AC_RATE * peakTimeMultiple;
+            totalCost += getPreRate(totalKms, PRE_RATE_CHANGE_AC_RATE, peakTimeMultiple);
+            totalCost += getPostRate(totalKms, POST_RATE_CHANGE_AC_RATE, peakTimeMultiple);
         } else {
-            totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
-            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
+            totalCost += getPreRate(totalKms, PRE_RATE_CHANGE_NON_AC_RATE, peakTimeMultiple);
+            totalCost += getPostRate(totalKms, POST_RATE_CHANGE_NON_AC_RATE, peakTimeMultiple);
         }
 
         return totalCost * (1 + SALES_TAX_RATE);
+    }
+
+    private double getPreRate(int totalKms, int preRate, double peakTimeMultiple) {
+        return Math.min(RATE_CHANGE_DISTANCE, totalKms) * preRate * peakTimeMultiple;
+    }
+
+    private double getPostRate(int totalKms, int preRate, double peakTimeMultiple) {
+        return Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * preRate * peakTimeMultiple;
     }
 }
