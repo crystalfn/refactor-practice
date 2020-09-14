d9bf7945eb5348d1a3d758723fb4b99110564ca2
crystalfn <969274162@qq.com>
2020-09-14 23:08:24 +0800

    [fangni] refactor: practice3

--- 3/src/main/java/com/twu/refactoring/Direction.java
+++ 3/src/main/java/com/twu/refactoring/Direction.java
@@ -1,40 +1,12 @@
 package com.twu.refactoring;
 
-public class Direction {
-    private final char direction;
+public abstract class Direction {
+    public char direction;
+    public abstract Direction turnRight();
+    public abstract Direction turnLeft();
 
-    public Direction(char direction) {
-        this.direction = direction;
-    }
-
-    public Direction turnRight() {
-        switch (direction) {
-            case 'N':
-                return new Direction('E');
-            case 'S':
-                return new Direction('W');
-            case 'E':
-                return new Direction('N');
-            case 'W':
-                return new Direction('S');
-            default:
-                throw new IllegalArgumentException();
-        }
-    }
-
-    public Direction turnLeft() {
-        switch (direction) {
-            case 'N':
-                return new Direction('W');
-            case 'S':
-                return new Direction('E');
-            case 'E':
-                return new Direction('N');
-            case 'W':
-                return new Direction('S');
-            default:
-                throw new IllegalArgumentException();
-        }
+    public char getDirection() {
+        return direction;
     }
 
     @Override
@@ -44,9 +16,7 @@ public class Direction {
 
         Direction direction1 = (Direction) o;
 
-        if (direction != direction1.direction) return false;
-
-        return true;
+        return direction == direction1.direction;
     }
 
     @Override
